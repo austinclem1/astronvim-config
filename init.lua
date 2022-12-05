5b27ce3a759a8b453ef3f12c70d6678c764f4c86
Austin Clements <austin.clements@smoothstack.com>
2022-12-05 16:41:25 -0600

    disable autoformat and force write shortcut

--- init.lua
+++ init.lua
@@ -3,22 +3,31 @@ local config = {
     opt = {
       scrolloff = 5,
     },
+    g = {
+      autoformat_enabled = false,
+    },
   },
 
   mappings = {
     n = {
+      -- Disable force quit and force write shortcuts
+      ["<C-s>"] = false,
+      ["<C-q>"] = false,
+      -- Use Alt-h and Alt-l for changing tabs
       ["<S-h>"] = false,
       ["<S-l>"] = false,
       ["<M-h>"] = { "<cmd>BufferLineCyclePrev<cr>", desc = "Previous buffer tab" },
       ["<M-l>"] = { "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer tab" },
     },
     v = {
+      -- Add alias motions as suggested by surround.nvim
       ["ir"] = { "i[", desc = "Inside square brackets" },
       ["ar"] = { "a[", desc = "Around square brackets" },
       ["ia"] = { "i<", desc = "Inside angle brackets" },
       ["aa"] = { "a<", desc = "Around angle brackets" },
     },
     o = {
+      -- Add alias motions as suggested by surround.nvim
       ["ir"] = { "i[", desc = "Inside square brackets" },
       ["ar"] = { "a[", desc = "Around square brackets" },
       ["ia"] = { "i<", desc = "Inside angle brackets" },
